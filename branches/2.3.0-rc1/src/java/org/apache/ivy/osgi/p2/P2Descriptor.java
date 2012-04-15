4ed6d7df37929fcbb7ee2269840cc0fa5f0773dc
maartenc <maartenc@13f79535-47bb-0310-9956-ffa450edef68>
2012-04-15 21:44:52 +0000

    Created 2.3.0 branch
    git-svn-id: https://svn.apache.org/repos/asf/ant/ivy/core@1326426 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/2.3.0-rc1/src/java/org/apache/ivy/osgi/p2/P2Descriptor.java
@@ -0,0 +1,83 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.ivy.osgi.p2;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ivy.osgi.core.BundleInfo;
+import org.apache.ivy.osgi.core.ExecutionEnvironmentProfileProvider;
+import org.apache.ivy.osgi.repo.RepoDescriptor;
+import org.apache.ivy.osgi.util.Version;
+
+public class P2Descriptor extends RepoDescriptor {
+
+    private long timestamp;
+
+    private Map/* <String, Map<Version, String>> */artifactUrlPatterns = new HashMap();
+
+    private String repoUrl;
+
+    public P2Descriptor(URI repoUri, ExecutionEnvironmentProfileProvider profileProvider) {
+        super(repoUri, profileProvider);
+        try {
+            repoUrl = repoUri.toURL().toExternalForm();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Illegal repo uri", e);
+        }
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void addBundle(BundleInfo bundleInfo) {
+        // before transforming it and adding it into the repo, let's add the artifacts
+        // and if no artifact, then no bundle
+
+        Map/* <Version, String> */urlPatternsByVersion = (Map) artifactUrlPatterns.get(bundleInfo
+                .getSymbolicName());
+        if (urlPatternsByVersion != null) {
+            String urlPattern = (String) urlPatternsByVersion.get(bundleInfo.getVersion());
+            if (urlPattern != null) {
+                String url = urlPattern.replaceAll("\\$\\{id\\}", bundleInfo.getSymbolicName());
+                url = url.replaceAll("\\$\\{version\\}", bundleInfo.getVersion().toString());
+                try {
+                    bundleInfo.setUri(new URI(url));
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException("Unable to build the artifact uri of " + bundleInfo,
+                            e);
+                }
+                super.addBundle(bundleInfo);
+            }
+        }
+    }
+
+    public void addArtifactUrl(String id, Version version, String url) {
+        Map/* <Version, String> */byVersion = (Map) artifactUrlPatterns.get(id);
+        if (byVersion == null) {
+            byVersion = new HashMap();
+            artifactUrlPatterns.put(id, byVersion);
+        }
+        byVersion.put(version, url);
+    }
+
+}
